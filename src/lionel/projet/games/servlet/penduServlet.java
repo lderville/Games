b1a03945f0a391d7ca62896fdca989f281ded1c7
lderville <91890830+lderville@users.noreply.github.com>
2022-01-08 19:41:23 +0100

    add

--- src/lionel/projet/games/servlet/penduServlet.java
+++ src/lionel/projet/games/servlet/penduServlet.java
@@ -1,7 +1,7 @@
 package lionel.projet.games.servlet;
 
 import java.io.IOException;
-import java.util.Iterator;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.jasper.tagplugins.jstl.core.ForEach;
+
 
 import lionel.projet.games.bll.Dictionnaire;
 
